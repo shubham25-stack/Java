6c77109078ffabf633fb49df79cf0ab337929e6b
Shubham Thakur <shubham.codecreator@gmail.com>
2025-10-25 23:38:24 +0530

    java learning

---
+++ first.java
@@ -0,0 +1,33 @@
+
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, java!");
+
+        // String name = "Shiv";
+        // int age = 21;
+        // String neighbor = "Shake";
+
+        // String friend = neighbor;
+
+        // System.out.printf("%s, %d, %s, %s", name, age, neighbor, friend);
+
+        // //primitive type datatypes
+        // byte age2 = 12;
+        // int phone = 1233221;
+        // long phone2 = 89299329399L;
+        // float pi = 3.14F;
+        // char letter = '$';
+        // boolean isAdult = false;
+
+        //non premetive
+        String name2 = "Ankush";
+        System.out.println(name2.length());
+        String n = "Shivu";
+        String n2 = n.replace('u','a');
+        System.out.println(n2);
+        
+        //substring
+        String a = "Shiv an Apple";
+        System.out.println(a.substring(0,4));
+    }
+} 
